668348e3e19b3d03d419db921ba2821cb417d71e
zhangshihan2023 <123339707+zhangshihan2023@users.noreply.github.com>
2023-02-08 10:39:12 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "Spencer's OneDrive",
+  title: "MEET EN PERSONAL ONEDRIVE",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/Public',
@@ -53,7 +53,11 @@ module.exports = {
   links: [
     {
       name: 'GitHub',
-      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
+      link: 'https://github.com/king-zhangbn',
+    },
+    {
+      name: 'Blog',
+      link: 'https://en.icu',
     },
   ],
 
